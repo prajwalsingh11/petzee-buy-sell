21ab9086fd5b680bd39dc0ebcb15dfbcaad0eff1
prajwalsingh11 <107928220+prajwalsingh11@users.noreply.github.com>
2024-03-10 19:21:44 +0530

    petzee

---
+++ includes/config.php
@@ -0,0 +1,18 @@
+<?php
+
+//Site
+$siteURL = "http://localhost/olx";
+$siteName = "olx";
+
+//DB 
+$dbHost = 'localhost';
+$dbUser = 'root';
+$dbPass = '';
+$dbName = 'petzee';
+
+//General Settings 
+$defaultLan = 'en';
+$defaultThemeColor = 'blue';
+
+
+?>
